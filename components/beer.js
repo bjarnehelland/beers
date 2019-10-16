32673ed2778fe9ae266036893059980194cda77d
Bjarne Helland <bjarnehelland@MacBook-Pro.localdomain>
2019-10-16 22:15:00 +0200

    initial commit

---
+++ components/beer.js
@@ -0,0 +1,70 @@
+import React from "react";
+import Like from "./like";
+import Interested from "./interested";
+
+const Beer = ({ beer, toggleLike, liked, toggleInterested, interested }) => (
+  <div className="beer">
+    <div className="like" onClick={toggleLike}>
+      <Like liked={liked} />
+    </div>
+    <div className="name" title={beer.description}>
+      {beer.name} ({beer.abv}%)
+    </div>
+    <div className="style">{beer.style}</div>
+    <div className="brewery">{beer.breweryName}</div>
+    <div className="room">{beer.room}</div>
+    <div className="rating">{beer.untappdRating}</div>
+    <div className="interested" onClick={toggleInterested}>
+      <Interested interested={interested} />
+    </div>
+    <style jsx>{`
+      .beer {
+        display: grid;
+        grid-template-columns: 15px 1fr 260px 275px 100px 42px 15px;
+        border-bottom: 1px solid #afafaf;
+      }
+      .beer > div {
+        display: flex;
+        align-items: center;
+        padding: 0 10px;
+      }
+      .beer > .rating {
+        justify-content: flex-end;
+      }
+
+      .beer > .like,
+      .beer > .interested {
+        padding: 0;
+      }
+
+      @media only screen and (max-width: 1000px) {
+        .beer {
+          grid-template-columns: 15px 1fr auto 15px;
+        }
+
+        .beer > .like {
+          grid-row-start: 1;
+          grid-row-end: 3;
+        }
+
+        .beer > .interested {
+          grid-row-start: 1;
+          grid-row-end: 3;
+          grid-column: 4;
+        }
+
+        .beer > div.style,
+        .beer > div.room {
+          justify-content: flex-end;
+          align-items: flex-start;
+        }
+
+        .beer > div.rating {
+          display: none;
+        }
+      }
+    `}</style>
+  </div>
+);
+
+export default Beer;
---
+++ pages/index.js
@@ -0,0 +1,114 @@
+import React from "react";
+import Head from "next/head";
+import fetch from "isomorphic-unfetch";
+import { firstBy } from "thenby";
+import Beer from "../components/beer";
+
+const sessionList = [
+  "Fredag (14-18)",
+  "Fredag (18-22)",
+  "Lørdag (14-18)",
+  "Lørdag (18-22)"
+];
+
+const Index = props => {
+  const [likes, setLikes] = React.useState({});
+  const [interested, setInterested]= React.useState({});
+
+  React.useEffect(() => {
+    if (Object.keys(likes).length === 0) return;
+    window.localStorage.setItem("likes", JSON.stringify(likes));
+  }, [likes]);
+
+  React.useEffect(() => {
+    if (Object.keys(interested).length === 0) return;
+    window.localStorage.setItem("interested", JSON.stringify(interested));
+  }, [interested]);
+
+  React.useEffect(() => {
+    const likes = localStorage.getItem("likes");
+    if (likes) setLikes(JSON.parse(likes));
+
+    const interested = localStorage.getItem("interested");
+    if (interested) setInterested(JSON.parse(interested));
+  }, []);
+
+  function handleToggleLike(name) {
+    setLikes(likes => ({ ...likes, [name]: likes[name] ? false : true }));
+  }
+
+  function handleToggleInterested(name) {
+    setInterested(interested => ({ ...interested, [name]: interested[name] ? false : true }));
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {sessionList.map(session => (
+        <div key={session}>
+          <h1>{session}</h1>
+          <div>
+            {props.sessions[session].map(beer => (
+              <Beer
+                key={beer.name}
+                beer={beer}
+                liked={!!likes[beer.name]}
+                interested={!!interested[beer.name]}
+                toggleLike={() => handleToggleLike(beer.name)}
+                toggleInterested={() => handleToggleInterested(beer.name)}
+              />
+            ))}
+          </div>
+        </div>
+      ))}
+
+      <style jsx>{`
+        h1 {
+          page-break-before: always;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+Index.getInitialProps = async () => {
+  const res = await fetch(
+    "https://www.whatsbrewing.no/wp-content/themes/wb2019/untappd/http/beers.json"
+  );
+  const json = await res.json();
+  const beers = json.map(b => ({
+    ...b,
+    session: getSession(b.untappdSectionId),
+    untappdRating: b.untappdRating > 0 ? b.untappdRating : null
+  }));
+
+  beers.sort(firstBy("room").thenBy("breweryName"));
+
+  const sessions = beers.reduce((sessions, beer) => {
+    if (!sessions[beer.session]) sessions[beer.session] = [];
+    sessions[beer.session].push(beer);
+    return sessions;
+  });
+  return { sessions };
+};
+
+function getSession(untappdSectionId) {
+  switch (untappdSectionId) {
+    case "440439":
+      return sessionList[0];
+    case "440440":
+      return sessionList[1];
+    case "440449":
+      return sessionList[2];
+    case "440450":
+      return sessionList[3];
+  }
+
+  return untappdSectionId;
+}
+
+export default Index;
